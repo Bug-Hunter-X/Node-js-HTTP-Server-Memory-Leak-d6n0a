a404c2277aa7478e103d332d4dd80c428c73ece0
BugHunter-X <askzenai@gmail.com>
2025-01-06 06:14:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+
+//This is a common error that results in memory leaks.  The solution requires closing the server when it's no longer needed.
